5b3563a21e2ca1afa9bad220dec3a6f1fd5bea83
Tanish2206 <tanishjain1023@gmail.com>
2023-10-24 14:12:12 +0530

    backend url updated

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     // backendBaseUrl: "http://13.233.39.177:3000/", // Replace with your backend base URL
-    backendBaseUrl: "http://localhost:3000/", // Replace with your backend base URL
+    backendBaseUrl: "https://backend-development-eta.vercel.app/", // Replace with your backend base URL
   },
 };
 
